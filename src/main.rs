f658e2a1beb6262f7c77914e82ce0d91f294a43c
Nathan Carver <nathan.carver@bionautlabs.com>
2022-08-02 18:31:19 -0400

    Added cucubmer base test file.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod item;
+use crate::item::{Item, GildedRose};
+
 fn main() {
     let mut items: Vec<Item> = Vec::new();
     items.push(Item {
@@ -67,87 +70,5 @@ fn main() {
         }
 
         println!("");
-    }
-}
-
-pub struct Item {
-    name: String,
-    sell_in: i32,
-    quality: u32,
-}
-
-pub struct GildedRose {
-    items: Vec<Item>,
-}
-
-impl GildedRose {
-    pub fn update_quality(&mut self) {
-        let mut i = 0;
-
-        loop {
-            if i >= self.items.len() {
-                break;
-            }
-
-            if !self.items[i].name.eq("Aged Brie")
-                && !self.items[i]
-                    .name
-                    .eq("Backstage passes to a TAFKAL80ETC concert")
-            {
-                if self.items[i].quality > 0 {
-                    if !self.items[i].name.eq("Sulfuras, Hand of Ragnaros") {
-                        self.items[i].quality = self.items[i].quality - 1;
-                    }
-                }
-            } else {
-                if self.items[i].quality < 50 {
-                    self.items[i].quality = self.items[i].quality + 1;
-
-                    if self.items[i]
-                        .name
-                        .eq("Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if self.items[i].sell_in < 11 {
-                            if self.items[i].quality < 50 {
-                                self.items[i].quality = self.items[i].quality + 1;
-                            }
-                        }
-
-                        if self.items[i].sell_in < 6 {
-                            if self.items[i].quality < 50 {
-                                self.items[i].quality = self.items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if !self.items[i].name.eq("Sulfuras, Hand of Ragnaros") {
-                self.items[i].sell_in = self.items[i].sell_in - 1;
-            }
-
-            if self.items[i].sell_in < 0 {
-                if !self.items[i].name.eq("Aged Brie") {
-                    if !self.items[i]
-                        .name
-                        .eq("Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if self.items[i].quality > 0 {
-                            if !self.items[i].name.eq("Sulfuras, Hand of Ragnaros") {
-                                self.items[i].quality = self.items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        self.items[i].quality = self.items[i].quality - self.items[i].quality;
-                    }
-                } else {
-                    if self.items[i].quality < 50 {
-                        self.items[i].quality = self.items[i].quality + 1;
-                    }
-                }
-            }
-
-            i += 1;
-        }
     }
 }
