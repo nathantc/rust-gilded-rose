6fe5a3f0d84a9839a96a21bc8e90f4d1b8a56b80
Nathan Carver <nathan.carver@bionautlabs.com>
2022-10-11 13:48:40 -0400

    Moved Item and GildedRose class into gilded_rose.rs. Added new method for Item struct.

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,17 @@
+use crate::gilded_rose::{GildedRose, Item};
+
+mod gilded_rose;
+
 fn main() {
     let mut items: Vec<Item> = Vec::new();
-    items.push(Item {
-        name: "+5 Dexterity Vest".to_string(),
-        sell_in: 10,
-        quality: 20,
-    });
-    items.push(Item {
-        name: "Aged Brea".to_string(),
-        sell_in: 2,
-        quality: 0,
-    });
-    items.push(Item {
-        name: "Elixir of the Mongoose".to_string(),
-        sell_in: 5,
-        quality: 7,
-    });
-    items.push(Item {
-        name: "Sulfuras, Hand of Ragnaros".to_string(),
-        sell_in: 0,
-        quality: 80,
-    });
-    items.push(Item {
-        name: "Backstage passes to a TAFKAL80ETC concert".to_string(),
-        sell_in: 15,
-        quality: 20,
-    });
-    items.push(Item {
-        name: "Backstage passes to a TAFKAL80ETC concert".to_string(),
-        sell_in: 10,
-        quality: 49,
-    });
-    items.push(Item {
-        name: "Backstage passes to a TAFKAL80ETC concert".to_string(),
-        sell_in: 5,
-        quality: 49,
-    });
-    items.push(Item {
-        name: "Conjured Mana Cake".to_string(),
-        sell_in: 3,
-        quality: 6,
-    });
+    items.push(Item::new("+5 Dexterity Vest",10, 20));
+    items.push(Item::new("Aged Brea", 2, 0,));
+    items.push(Item::new("Elixir of the Mongoose", 5, 7,));
+    items.push(Item::new("Sulfuras, Hand of Ragnaros", 0, 80,));
+    items.push(Item::new("Backstage passes to a TAFKAL80ETC concert", 15, 20,));
+    items.push(Item::new("Backstage passes to a TAFKAL80ETC concert", 10, 49,));
+    items.push(Item::new("Backstage passes to a TAFKAL80ETC concert", 5, 49,));
+    items.push(Item::new("Conjured Mana Cake", 3, 6,));
 
     let mut gilded_rose = GildedRose { items: items };
 
@@ -67,87 +39,5 @@ fn main() {
         }
 
         println!("");
-    }
-}
-
-pub struct Item {
-    name: String,
-    sell_in: i32,
-    quality: u32,
-}
-
-pub struct GildedRose {
-    items: Vec<Item>,
-}
-
-impl GildedRose {
-    pub fn update_quality(&mut self) {
-        let mut i = 0;
-
-        loop {
-            if i >= self.items.len() {
-                break;
-            }
-
-            if !self.items[i].name.eq("Aged Brie")
-                && !self.items[i]
-                    .name
-                    .eq("Backstage passes to a TAFKAL80ETC concert")
-            {
-                if self.items[i].quality > 0 {
-                    if !self.items[i].name.eq("Sulfuras, Hand of Ragnaros") {
-                        self.items[i].quality = self.items[i].quality - 1;
-                    }
-                }
-            } else {
-                if self.items[i].quality < 50 {
-                    self.items[i].quality = self.items[i].quality + 1;
-
-                    if self.items[i]
-                        .name
-                        .eq("Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if self.items[i].sell_in < 11 {
-                            if self.items[i].quality < 50 {
-                                self.items[i].quality = self.items[i].quality + 1;
-                            }
-                        }
-
-                        if self.items[i].sell_in < 6 {
-                            if self.items[i].quality < 50 {
-                                self.items[i].quality = self.items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if !self.items[i].name.eq("Sulfuras, Hand of Ragnaros") {
-                self.items[i].sell_in = self.items[i].sell_in - 1;
-            }
-
-            if self.items[i].sell_in < 0 {
-                if !self.items[i].name.eq("Aged Brie") {
-                    if !self.items[i]
-                        .name
-                        .eq("Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if self.items[i].quality > 0 {
-                            if !self.items[i].name.eq("Sulfuras, Hand of Ragnaros") {
-                                self.items[i].quality = self.items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        self.items[i].quality = self.items[i].quality - self.items[i].quality;
-                    }
-                } else {
-                    if self.items[i].quality < 50 {
-                        self.items[i].quality = self.items[i].quality + 1;
-                    }
-                }
-            }
-
-            i += 1;
-        }
     }
 }
